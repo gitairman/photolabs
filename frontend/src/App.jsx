da7910bac7203de0ca060688487c7cc2683bb2ca
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-16 12:03:06 -0700

    render photoList component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,7 @@ import React from 'react';
 
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
-
-const sampleDataForPhotoListItem = {
-  id: '1',
-  location: {
-    city: 'Montreal',
-    country: 'Canada',
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: 'Joe Example',
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
+import PhotoList from 'components/PhotoList';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -21,9 +11,10 @@ const App = () => {
 
   return (
     <div className="App">
-      {photos.map((_, i) => (
+      {/* {photos.map((_, i) => (
         <PhotoListItem key={i} {...sampleDataForPhotoListItem} />
-      ))}
+      ))} */}
+      <PhotoList />
     </div>
   );
 };
--- frontend/src/components/PhotoList.jsx
+++ frontend/src/components/PhotoList.jsx
@@ -1,56 +1,57 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/PhotoList.scss";
+import '../styles/PhotoList.scss';
+import PhotoListItem from './PhotoListItem';
 
 const sampleDataForPhotoList = [
   {
-    id: "1",
+    id: '1',
     location: {
-      city: "Montreal",
-      country: "Canada",
+      city: 'Montreal',
+      country: 'Canada',
     },
     urls: {
       full: `${process.env.PUBLIC_URL}/Image-1-Full.jpeg`,
       regular: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
     },
     user: {
-      id: "1",
-      username: "exampleuser",
-      name: "Joe Example",
+      id: '1',
+      username: 'exampleuser',
+      name: 'Joe Example',
       profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
     },
   },
   {
-    id: "2",
+    id: '2',
     location: {
-      city: "Toronto",
-      country: "Canada",
+      city: 'Toronto',
+      country: 'Canada',
     },
     urls: {
       full: `${process.env.PUBLIC_URL}/Image-2-Full.jpeg`,
       regular: `${process.env.PUBLIC_URL}/Image-2-Regular.jpeg`,
     },
     user: {
-      id: "2",
-      username: "exampleuser",
-      name: "Joe Example",
+      id: '2',
+      username: 'exampleuser',
+      name: 'Joe Example',
       profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
     },
   },
   {
-    id: "3",
+    id: '3',
     location: {
-      city: "Ottawa",
-      country: "Canada",
+      city: 'Ottawa',
+      country: 'Canada',
     },
     urls: {
       full: `${process.env.PUBLIC_URL}/Image-3-Full.jpeg`,
       regular: `${process.env.PUBLIC_URL}/Image-3-Regular.jpeg`,
     },
     user: {
-      id: "3",
-      username: "exampleuser",
-      name: "Joe Example",
+      id: '3',
+      username: 'exampleuser',
+      name: 'Joe Example',
       profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
     },
   },
@@ -59,7 +60,16 @@ const sampleDataForPhotoList = [
 const PhotoList = () => {
   return (
     <ul className="photo-list">
-      {/* Insert React */}
+      {sampleDataForPhotoList.map((p, i) => (
+        <li key={i}>
+          <PhotoListItem
+            location={p.location}
+            imageSource={p.urls.full}
+            username={p.user.username}
+            profile={p.user.profile}
+          />
+        </li>
+      ))}
     </ul>
   );
 };
