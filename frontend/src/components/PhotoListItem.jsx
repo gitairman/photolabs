b85a0118a1b3d6d49595937afcbb05a76c4dbcb2
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-16 09:25:22 -0700

    added more classes to photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -5,13 +5,17 @@ import '../styles/PhotoListItem.scss';
 const PhotoListItem = (props) => {
   const { id, location, imageSource, username, profile } = props;
   return (
-    <div>
-      <img className="photo-list__item" src={imageSource} />
-      <img src={profile} />
-      <p>{username}</p>
-      <p>
-        {location.city}, {location.country}
-      </p>
+    <div className="photo-list__item">
+      <img className="photo-list__image" src={imageSource} />
+      <div className="photo-list__user-profile">
+        <img className="photo-list__image" src={profile} />
+        <div className="photo-list__user-details">
+          <p className="photo-list__user-info">{username}</p>
+          <p className="photo-list__user-location">
+            {location.city}, {location.country}
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
