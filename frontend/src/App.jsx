d7190a7d0afe2ed449951154e04f4f186f2b55fc
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-16 12:47:45 -0700

    added HomeRoute

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
 
 import './App.scss';
-import PhotoList from 'components/PhotoList';
-import TopNavigation from 'components/TopNavigationBar';
+import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -11,8 +10,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <TopNavigation />
-      <PhotoList />
+      <HomeRoute />
     </div>
   );
 };
