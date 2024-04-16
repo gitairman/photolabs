d3b720d67c17c12cc607878dfec7d3901f56f614
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-16 12:43:44 -0700

    added topnavigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 
 import './App.scss';
-// import PhotoListItem from './components/PhotoListItem';
 import PhotoList from 'components/PhotoList';
-// import TopicListItem from 'components/TopicListItem';
-import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
@@ -13,7 +11,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
