8094c4deb8fa10c1922fcc7a6dacfefcec2c0b52
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-16 08:58:56 -0700

    used props for photolistitem

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,21 +1,19 @@
-import React from "react";
+import React from 'react';
 
-import "../styles/PhotoListItem.scss";
+import '../styles/PhotoListItem.scss';
 
-
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-const PhotoListItem = () => {
-  /* Insert React */
+const PhotoListItem = (props) => {
+  const { id, location, imageSource, username, profile } = props;
+  return (
+    <div>
+      <img src={imageSource} />
+      <img src={profile} />
+      <p>{username}</p>
+      <p>
+        {location.city}, {location.country}
+      </p>
+    </div>
+  );
 };
 
 export default PhotoListItem;
