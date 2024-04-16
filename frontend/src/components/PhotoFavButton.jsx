965b35e046f84b300ea33dcaeaed497d245bf4b9
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-16 11:12:13 -0700

    added like button and made it change state on click

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,16 +1,19 @@
-import React, { useCallback, useState } from 'react';
+import React, { useState } from 'react';
 
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
-function PhotoFavButton() {
+const PhotoFavButton = () => {
+  const [selected, setSelected] = useState(false);
   return (
-    <div className="photo-list__fav-icon">
+    <div
+      onClick={() => setSelected(!selected)}
+      className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon displayAlert={false} selected={selected} />
       </div>
     </div>
   );
-}
+};
 
 export default PhotoFavButton;
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import '../styles/PhotoListItem.scss';
+import PhotoFavButton from './PhotoFavButton';
 
 const PhotoListItem = (props) => {
   const { id, location, imageSource, username, profile } = props;
   return (
     <div className="photo-list__item">
+      <PhotoFavButton />
       <img className="photo-list__image" src={imageSource} />
       <div className="photo-list__user-details">
         <img className="photo-list__user-profile" src={profile} />
