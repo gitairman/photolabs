9f81c1a6473b6988c249b8f4d3d2e25939463c9e
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-16 23:13:44 -0700

    removed state logic from homeroute

--- frontend/src/routes/HomeRoute.jsx
+++ frontend/src/routes/HomeRoute.jsx
@@ -7,26 +7,6 @@ import PhotoDetailsModal from './PhotoDetailsModal';
 import { useApplicationData } from 'hooks/useApplicationData';
 
 const HomeRoute = ({ photos, topics }) => {
-  // const [favPhotos, setFavPhotos] = useState([]);
-  // const handleFavourite = (id) =>
-  //   isAlreadyFavourite(id) ? removeFromFavourites(id) : addToFavourites(id);
-
-  // const isAlreadyFavourite = (id) => favPhotos.includes(id);
-  // const addToFavourites = (id) => setFavPhotos([...favPhotos, id]);
-  // const removeFromFavourites = (id) =>
-  //   setFavPhotos(favPhotos.filter((p) => p !== id));
-
-  // const [showModal, setShowModal] = useState(false);
-  // const [singlePhotoDetail, setSinglePhotoDetail] = useState({});
-  // const handleClickPhoto = (id) => {
-  //   setSinglePhotoDetail(photos.find((p) => p.id === id));
-  //   setShowModal(true);
-  // };
-
-  // const closeModal = () => {
-  //   setShowModal(false);
-  // };
-
   const {
     state,
     onPhotoSelect,
