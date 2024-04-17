67b555db3dc607730a9bf2b51dd3d0d6e53cb6d0
Aaron Hopkins <aaronhopkins86@gmail.com>
2024-04-17 08:48:09 -0700

    refactored to use useReducer

--- frontend/src/hooks/useApplicationData.js
+++ frontend/src/hooks/useApplicationData.js
@@ -1,30 +1,80 @@
 import photos from 'mocks/photos';
 import topics from 'mocks/topics';
 
-const { useState } = require('react');
+const { useReducer } = require('react');
+
+export const ACTIONS = {
+  ADD_FAV_PHOTO: 'ADD_FAV_PHOTO',
+  REMOVE_FAV_PHOTO: 'REMOVE_FAV_PHOTO',
+  SET_PHOTO_DATA: 'SET_PHOTO_DATA',
+  SET_TOPIC_DATA: 'SET_TOPIC_DATA',
+  SELECT_PHOTO: 'SELECT_PHOTO',
+  DISPLAY_PHOTO_DETAILS: 'DISPLAY_PHOTO_DETAILS',
+};
+
+const initialState = {
+  favPhotos: [],
+  showModal: false,
+  singlePhotoDetail: {},
+  photos,
+  topics,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+  case 'ADD_FAV_PHOTO':
+    return { ...state, favPhotos: [...state.favPhotos, action.payload] };
+  case 'REMOVE_FAV_PHOTO':
+    return {
+      ...state,
+      favPhotos: state.favPhotos.filter((p) => p !== action.payload),
+    };
+  case 'SET_PHOTO_DATA':
+    return { ...state, photos: action.payload };
+  case 'SET_TOPIC_DATA':
+    return { ...state, topics: action.payload };
+  case 'SELECT_PHOTO':
+    return {
+      ...state,
+      singlePhotoDetail: state.photos.find((p) => p.id === action.payload),
+    };
+  case 'DISPLAY_PHOTO_DETAILS':
+    return { ...state, showModal: action.payload };
+  case 'CLOSE_PHOTO_DETAILS':
+    return { ...state, showModal: action.payload };
+  default:
+    return state;
+  }
+};
 
 export const useApplicationData = () => {
-  const [favPhotos, setFavPhotos] = useState([]);
+  const [state, dispatch] = useReducer(reducer, initialState);
+
   const handleFavourite = (id) =>
     isAlreadyFavourite(id) ? removeFromFavourites(id) : addToFavourites(id);
 
-  const isAlreadyFavourite = (id) => favPhotos.includes(id);
-  const addToFavourites = (id) => setFavPhotos([...favPhotos, id]);
-  const removeFromFavourites = (id) =>
-    setFavPhotos(favPhotos.filter((p) => p !== id));
+  const isAlreadyFavourite = (id) => state.favPhotos.includes(id);
 
-  const [showModal, setShowModal] = useState(false);
-  const [singlePhotoDetail, setSinglePhotoDetail] = useState({});
+  const addToFavourites = (id) =>
+    dispatch({ type: 'ADD_FAV_PHOTO', payload: id });
+
+  const removeFromFavourites = (id) =>
+    dispatch({ type: 'REMOVE_FAV_PHOTO', payload: id });
+
+
   const handleClickPhoto = (id) => {
-    setSinglePhotoDetail(photos.find((p) => p.id === id));
-    setShowModal(true);
+ 
+    dispatch({ type: 'SELECT_PHOTO', payload: id });
+
+    dispatch({ type: 'DISPLAY_PHOTO_DETAILS', payload: true });
   };
 
   const closeModal = () => {
-    setShowModal(false);
+
+    dispatch({ type: 'CLOSE_PHOTO_DETAILS', payload: false });
   };
 
-  const state = { photos, topics, favPhotos, showModal, singlePhotoDetail };
+
   const onPhotoSelect = handleClickPhoto;
   const updateToFavPhotoIds = handleFavourite;
   const onLoadTopic = () => {};
